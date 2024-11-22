8254008f7528a38f73dfa68814c78ce45eff0d28
LyneSultan <lynesultane@gmail.com>
2024-11-23 00:44:13 +0200

    created signup component

---
+++ app/src/components/Signup.jsx
@@ -0,0 +1,34 @@
+const Signup = () => {
+
+  return (<>
+    <div>
+      <label>Name</label>
+      <input
+        type="text"
+        name="input1"
+      />
+    </div>
+    <div>
+      <label>Email</label>
+      <input
+        type="text"
+      />
+    </div>
+    <div>
+      <label>Password</label>
+      <input
+        type="password"
+      />
+    </div>
+    <div>
+      <label>Confirm password</label>
+      <input
+        type="password"
+      />
+    </div>
+    <button>Sign in</button>
+  </>)
+
+
+}
+export default Signup;
