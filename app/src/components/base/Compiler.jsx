a43f13de9ac4ce31a704fca06e6c891ec6c79685
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-24 00:37:57 +0200

    js<>fixed the error of editor window resizing

--- app/src/components/base/Compiler.jsx
+++ app/src/components/base/Compiler.jsx
@@ -4,6 +4,24 @@ import Editor from "@monaco-editor/react";
 import { Play } from "lucide-react";
 
 const Compiler = () => {
+  const editorRef = useRef(null);
+
+  const handleMount = (editor) => {
+    editorRef.current = editor;
+  };
+  useEffect(() => {
+    const handleResize = () => {
+      if (editorRef.current) {
+        editorRef.current.layout();
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div>
       hi
