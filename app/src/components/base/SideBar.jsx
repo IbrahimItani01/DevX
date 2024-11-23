758cb7f4145208959ba9c5290dc0c2106b11265b
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-23 19:40:10 +0200

    jsx<>finalized the sidebar component

--- app/src/components/base/SideBar.jsx
+++ app/src/components/base/SideBar.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import blackLogo from "../../assets/logoBlack.png"
 import FileAdd from '../FileAdd'
+import FileContainer from './FileContainer'
+import "../../styles/sidebar.css"
+import SignOut from './SignOut'
 const SideBar = () => {
   return (
     <div className='side-bar'>
       <div className='file-add'>
         <img alt='logo' src={blackLogo} width={50}></img>
         <FileAdd/>
+      </div>
+      <div className='files-list'>
+        <FileContainer/>
+        <FileContainer/>
+        <FileContainer/>
+        <FileContainer/>
+        <FileContainer/>
+        <FileContainer/>
+      </div>
+      <div className='signout'>
+        <SignOut/>
       </div>
     </div>
   )
