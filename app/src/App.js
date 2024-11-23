18b664e627686afad5f1bfa848930db2789e616a
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-23 03:29:40 +0200

    Commited

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignForm from './components/SignForm';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignForm />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
