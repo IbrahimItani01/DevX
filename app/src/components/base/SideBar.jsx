6d6270922a6dd4a3e2450be552c2e45411201c2d
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-23 17:01:13 +0200

    jsx<>implemented the fileAdd component in side bar

--- app/src/components/base/SideBar.jsx
+++ app/src/components/base/SideBar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import blackLogo from "../../assets/logoBlack.png"
+import FileAdd from '../FileAdd'
 const SideBar = () => {
   return (
-    <div>
-      
+    <div className='side-bar'>
+      <div className='file-add'>
+        <img alt='logo' src={blackLogo} width={50}></img>
+        <FileAdd/>
+      </div>
     </div>
   )
 }
