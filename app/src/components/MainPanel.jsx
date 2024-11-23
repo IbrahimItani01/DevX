37bdb14b034f3c6a1937ec6b9a1b699861151ffb
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-23 17:00:45 +0200

    jsx<>wrapped components in divs for style structure

--- app/src/components/MainPanel.jsx
+++ app/src/components/MainPanel.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import SideBar from './base/SideBar'
 import Header from './base/Header'
 import Compiler from './base/Compiler'
-
+import "../styles/mainpanel.css"
 const MainPanel = () => {
   return (
-    <>
+    <div className='main-panel'>
       <SideBar/>
-      <Header/>
-      <Compiler/>
-    </>
+      <div className='body-panel'>
+        <Header/>
+        <Compiler/>
+      </div>
+    </div>
   )
 }
 
