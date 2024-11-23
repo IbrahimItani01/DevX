38768b047bb7b12ab2712558081e35e869004108
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-23 23:32:06 +0200

    jsx<>finalized the structure and props of RoleIcon component

--- app/src/components/base/RoleIcon.jsx
+++ app/src/components/base/RoleIcon.jsx
@@ -1,12 +1,19 @@
-import { Eye, Pencil } from 'lucide-react'
-import React from 'react'
+import { Eye, Pencil } from "lucide-react";
+import React from "react";
 
-const RoleIcon = ({role}) => {
+const RoleIcon = ({ role, isActive, onClick }) => {
   return (
-    <div className='role-button'>
-      {role==="edit"?<Pencil/>:<Eye/>}
+    <div
+      className={`role-button ${isActive ? "active" : ""}`}
+      onClick={onClick}
+    >
+      {role === "edit" ? (
+        <Pencil className={isActive ? "active" : ""} />
+      ) : (
+        <Eye className={isActive ? "active" : ""} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default RoleIcon
+export default RoleIcon;
