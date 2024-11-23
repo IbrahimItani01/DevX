883961853098d1998034120df25779e37d58b06a
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-23 03:40:07 +0200

    jsx<>created a template for SignForm

--- app/src/components/SignForm.jsx
+++ app/src/components/SignForm.jsx
@@ -1,25 +1,14 @@
 import './../styles/base.css';
 import './../styles/signup.css';
-import FormInput from './FormInput';
-import Button from './base/Button';
-const SignForm = () => {
-
+import coloredLogo from "../logo.svg"
+const SignForm = ({children}) => {
   return (<>
-    <div className="signForm flex column align-center">
-      <div className='row flex justify-center'>
-        <FormInput label="Name" type="text" name="text" placeholder="Enter your name" />
-        <FormInput label="Email" type="text" name="email" placeholder="Enter your email" />
+    <div className="main signForm flex column align-center">
+      <div className='sign-title'>
+        <img alt='logo' src={coloredLogo} width={50}></img>
+        <h1>Welcome to DevX</h1>
       </div>
-
-      <div className='row flex justify-center'>
-        <FormInput label="Password" type="password" name="password" placeholder="Enter your password" />
-        <FormInput label="Confirm Password" type="password" name="text" placeholder="Confirm your password" />
-      </div>
-      {/* <div><button>Sign in</button></div> */}
-      <div><Button text="Sign up" /></div>
-
-      <p>Have an Account? LOGIN</p>
-
+     {children}
     </div>
   </>)
 
