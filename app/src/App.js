d185a25f4bab994fa2ecb160652c9ee24b33832b
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-23 02:33:55 +0200

    js <> Edited the App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
+import './styles/Login.css';
+import Login from './components/Login';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <Login />
+    </div>
   );
 }
 
