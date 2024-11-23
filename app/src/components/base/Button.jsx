6540d93c5ead5c4751f5e893ef18ad4bd9d0ab8a
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-23 23:28:31 +0200

    jsx<>added disabled attribute for the Button component

--- app/src/components/base/Button.jsx
+++ app/src/components/base/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 
-const Button = ({ text, onClick }) => {
+const Button = ({ text, onClick,disabled=false }) => {
   return (
-    <button className="base-button" onClick={() => onClick()}>
+    <button className="base-button" onClick={() => onClick()} disabled={disabled}>
       {text}
     </button>
   );
