679aa55d63f966ccb21d975a71a311e8679c1b52
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-23 21:12:04 +0200

    jsx<>completed header section jsx

--- app/src/components/base/Header.jsx
+++ app/src/components/base/Header.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { userContext } from '../../context/UserContext'
+import InviteField from './InviteField';
 
 const Header = () => {
+  const userData = useContext(userContext);
   return (
-    <div>
-      
+    <div className='header-section'>
+      <h1>🔥 Welcome Back, {userData.name}!</h1>
+      <div className='invite-section'>
+        <InviteField/>
+      </div>
     </div>
   )
 }
