054cd4762010c2d63411fa70adc0ef3453a9e107
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-23 20:17:06 +0200

    jsx<>implemented the add file logic to append the filesdata array

--- app/src/components/FileAdd.jsx
+++ app/src/components/FileAdd.jsx
@@ -1,16 +1,37 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import {CirclePlus} from "lucide-react";
+import { filesContext } from '../context/FilesContext';
 const FileAdd = () => {
     const [fileName,setFileName] = useState("");
+    const [alert,setAlert]=useState(false);
+    const {setFilesData}=useContext(filesContext);
     const handleChange = (e)=>{
         setFileName(e.target.value);
     }
     const handleFileCreate = ()=>{
-        
+        if(fileName){
+          // TODO: handle file create api
+          setFilesData((prev)=>[
+            ...prev,
+            {
+              id:2,
+              fileName
+            }
+          ]
+           
+          )
+          setAlert(false);
+          setFileName("");
+        }
+        else{
+          setAlert(true);
+        }
+
     }
   return (
     <div className='add-file'>
       <input placeholder='File Name ...' type='text' onChange={handleChange} value={fileName}></input>
+      {alert && <em>Note! Enter a file name ... </em>}
       <CirclePlus width={30} onClick={handleFileCreate} color='black' style={{cursor:"pointer"}}/>
     </div>
   )
