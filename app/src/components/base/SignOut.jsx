c093764a05a1723681e78a054798cd55fb04a312
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-24 02:19:33 +0200

    js<>handled the signout logic from auth provider

--- app/src/components/base/SignOut.jsx
+++ app/src/components/base/SignOut.jsx
@@ -1,12 +1,15 @@
 import { LogOut } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
 const SignOut = () => {
     const navigate = useNavigate();
+    const {logout}=useAuth();
+
     const handleSignOut = ()=>{
         localStorage.clear();
         navigate("/login");
-
+        logout();
     }
   return (
     <LogOut onClick={handleSignOut} color='black'/>
