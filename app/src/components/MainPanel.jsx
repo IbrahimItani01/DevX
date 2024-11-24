f6578099ad27d802027542c0a400111c41e30bfb
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-24 20:55:42 +0200

    jsx<>rendered Route components with compiler element based on files Data

--- app/src/components/MainPanel.jsx
+++ app/src/components/MainPanel.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import SideBar from './base/SideBar'
-import Header from './base/Header'
-import Compiler from './base/Compiler'
-import "../styles/mainpanel.css"
+import React, { useContext } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import SideBar from "./base/SideBar";
+import Header from "./base/Header";
+import Compiler from "./base/Compiler";
+import "../styles/mainpanel.css";
+import { filesContext } from "../context/FilesContext";
+
 const MainPanel = () => {
+  const { filesData, selectedFileId } = useContext(filesContext);
+
   return (
-    <div className='main-panel'>
-      <SideBar/>
-      <div className='body-panel'>
-        <Header/>
-        <Compiler/>
+    <div className="main-panel">
+      <SideBar />
+      <div className="body-panel">
+        <Header />
+        <Routes>
+
+          {filesData.map((file) => (
+            <Route
+              key={file.id}
+              path={`${file.id}`}
+              element={<Compiler file={file} />}
+            />
+          ))}
+        </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainPanel
+export default MainPanel;
