9aa6eea347d7f9982cd82c6ccb648fdd892f3082
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-24 20:57:07 +0200

    js<>provided selected file stat ein files context

--- app/src/context/FilesContext.js
+++ app/src/context/FilesContext.js
@@ -1,19 +1,23 @@
-import { createContext, useState } from "react";
+import React, { createContext, useState } from "react";
 
 export const filesContext = createContext();
 
-const FilesProvider = ({children})=>{
-    const [filesData,setFilesData]=useState([]);
-    // TODO: handle the get files data api
-    return (
-        <filesContext.Provider value={{
-            filesData,
-            setFilesData
-        }
-        }>
-            {children}
-        </filesContext.Provider>
-    );
-}
+const FilesProvider = ({ children }) => {
+  const [filesData, setFilesData] = useState([
+    { id: "1", fileName: "File 1"},
+    { id: "2", fileName: "File 2"},
+  ]);
+  const [selectedFileId, setSelectedFileId] = useState(null);
+
+  const selectFile = (id) => setSelectedFileId(id);
+
+  return (
+    <filesContext.Provider
+      value={{ filesData, setFilesData, selectedFileId, selectFile }}
+    >
+      {children}
+    </filesContext.Provider>
+  );
+};
 
 export default FilesProvider;
