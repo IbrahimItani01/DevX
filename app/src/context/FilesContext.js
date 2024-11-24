89b24c35d61ca29ec6244a6c70eddad0e478ee61
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-25 00:05:14 +0200

    js<>added logic in files context

--- app/src/context/FilesContext.js
+++ app/src/context/FilesContext.js
@@ -4,16 +4,28 @@ export const filesContext = createContext();
 
 const FilesProvider = ({ children }) => {
   const [filesData, setFilesData] = useState([
-    { id: "1", fileName: "File 1"},
-    { id: "2", fileName: "File 2"},
+    { id: "1", fileName: "File1", content: "" },
+    { id: "2", fileName: "File2", content: "" },
   ]);
   const [selectedFileId, setSelectedFileId] = useState(null);
 
-  const selectFile = (id) => setSelectedFileId(id);
+  const selectFile = (fileId) => {
+    setSelectedFileId(fileId);
+  };
+
+  const saveContent = (fileId, newContent) => {
+    setFilesData((prevFiles) =>
+      prevFiles.map((file) =>
+        file.id === fileId ? { ...file, content: newContent } : file
+      )
+    );
+    // Sync with database
+    console.log(`Saving file ${fileId}:`, newContent);
+  };
 
   return (
     <filesContext.Provider
-      value={{ filesData, setFilesData, selectedFileId, selectFile }}
+      value={{ filesData, selectedFileId, selectFile, saveContent }}
     >
       {children}
     </filesContext.Provider>
