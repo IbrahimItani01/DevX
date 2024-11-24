bf126ec2fd2c7e9244fd167c7e00c6615c5e56f2
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-24 18:38:26 +0200

    js<>implemented the compile api fetch in a separate file

---
+++ app/src/apis/compile.js
@@ -0,0 +1,21 @@
+import axios from "axios"
+import { languageVersions } from "../constants"
+
+const api = axios.create({
+    baseURL:"https://emkc.org/api/v2/piston"
+})
+
+export const executeCode = async (lang, code, stdin = "") => {
+    const response = await api.post("/execute", {
+      language: lang,
+      version: languageVersions[lang],
+      files: [
+        {
+          content: code,
+        },
+      ],
+      stdin,
+    });
+    return response.data;
+  };
+  
