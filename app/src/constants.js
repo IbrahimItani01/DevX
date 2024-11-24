b89eaa9a5b126212808dccfb661391cdebeb775c
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-24 18:37:54 +0200

    js<>added constants to the constants file

--- app/src/constants.js
+++ app/src/constants.js
@@ -1,5 +1,12 @@
 export const languageVersions = {
-    javascript:"10.15.0",
+    javascript:"18.15.0",
     python:"3.10.0",
     php:"8.2.3",
 }
+export const snippets = {
+    javascript: `//life is short, write a code`,
+    php: `<?php\n/*Life is short, write a code*/`,
+    python: `#Life is short, write a code `
+}
+export const defaultOutput = "Run Code to see result ...";
+export const defaultLanguage = "javascript";
