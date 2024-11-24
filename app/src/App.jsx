ce1f0f7cbbd50ee738ed04ce2668488f80f946e3
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-24 02:18:06 +0200

    jsx<>wrapped the App with AuthProvider and panel with Protected route component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,32 +1,50 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import SignForm from './components/SignForm';
+import SignForm from "./components/SignForm";
 import MainPanel from "./components/MainPanel";
-import "./styles/layout.css"
+import "./styles/layout.css";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import FilesProvider from "./context/FilesContext";
 import UserProvider from "./context/UserContext";
+import { AuthProvider } from "./context/AuthContext.js";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignForm>
-            <Register/>
-        </SignForm>}/>
-            <Route path="/login" element={<SignForm>
-            <Login/>
-        </SignForm>} />
-        
-        <Route path="/panel" element={
-          <FilesProvider>
-            <UserProvider>
-              <MainPanel/>
-            </UserProvider>
-          </FilesProvider>
-          }/>
-      </Routes>
-    </BrowserRouter>
-
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <SignForm>
+                <Register />
+              </SignForm>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <SignForm>
+                <Login />
+              </SignForm>
+            }
+          />
+          <Route
+            path="/panel"
+            element={
+              <ProtectedRoute>
+                <FilesProvider>
+                  <UserProvider>
+                    <MainPanel />
+                  </UserProvider>
+                </FilesProvider>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
