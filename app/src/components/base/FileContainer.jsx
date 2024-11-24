571f2886e9df5b90853a3dcb4d0d86c92e1cee73
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-24 20:56:44 +0200

    jsx<>pushed the active state upward to side bas rather than fileContainer component

--- app/src/components/base/FileContainer.jsx
+++ app/src/components/base/FileContainer.jsx
@@ -1,23 +1,20 @@
-import React, {useState } from 'react'
-import {ArrowRight, File} from 'lucide-react';
-const FileContainer = ({name,collabs=0}) => {
-  const [active,setActive] = useState(false)
-  const handleFileOpen = ()=>{
-    setActive(!active);
-  }
-  return (
-    <div onClick={handleFileOpen} className='file-container'>
-      <div className='file-info'>
-          <div>
-            <File color='black' />
-            <p>{name}</p>
-            {active && <ArrowRight color='black'/>}
-          </div>
-          {collabs>0 && <em>{collabs}</em> }
-      </div>
-      <div className='separator'></div>
-    </div>
-  )
-}
+import React from "react";
+import { ArrowRight, File } from "lucide-react";
 
-export default FileContainer
+const FileContainer = ({ name, collabs = 0, onClick, active }) => {
+  return (
+    <div onClick={onClick} className="file-container">
+      <div className="file-info">
+        <div>
+          <File color="black" />
+          <p>{name}</p>
+          {active && <ArrowRight color="black" />} {/* Show arrow if active */}
+        </div>
+        {collabs > 0 && <em>{collabs}</em>}
+      </div>
+      <div className="separator"></div>
+    </div>
+  );
+};
+
+export default FileContainer;
