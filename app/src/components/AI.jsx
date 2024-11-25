ef9cf5d6fef151af9ccbf1476b02e2e80561d53d
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-25 23:16:28 +0200

    js<>initialized the states in the AI component

--- app/src/components/AI.jsx
+++ app/src/components/AI.jsx
@@ -1,3 +1,13 @@
+import { Bug } from "lucide-react";
+import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const AI = ({ script,setScript }) => {
+  const [clicked, setClicked] = useState(false);
+  const [action, setAction] = useState("");
+    const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+  const handleClick = () => setClicked(!clicked);
   return (
     <>
       <div className="ai-button" onClick={handleClick}>
