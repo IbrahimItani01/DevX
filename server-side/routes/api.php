07f60420fbedb73d1a98d0048b4dd450cdede4c4
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-25 22:55:18 +0200

    php <> Finalized Middleware

--- server-side/routes/api.php
+++ server-side/routes/api.php
@@ -6,29 +6,18 @@ use App\Http\Controllers\EmailController;
 use App\Http\Controllers\AuthController;
 use App\Http\Middleware\JwtMiddleware;
 
-Route::post('/login', [AuthController::class, 'login']); // Login and generate token
 
-Route::get('/me', [AuthController::class, 'getUser'])->middleware('auth.jwt'); // Get authenticated user
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/debug-token', function (\Illuminate\Http\Request $request) {
-    return response()->json([
-        'authorization_header' => $request->header('Authorization'),
-        'token' => \Tymon\JWTAuth\Facades\JWTAuth::getToken(),
-    ]);
-});
-
-Route::group(['middleware' => [JwtMiddleware::class]], function () {
-    Route::get('/user', function () {
-        // This route is protected by JWT Middleware
-        return response()->json(['message' => 'You are authorized!']);
-    });
-
-    Route::get('/profile', function () {
-        // Another protected route
-        return response()->json(['message' => 'This is your profile!']);
+// Protected routes
+Route::middleware(['jwt'])->group(function () {
+    Route::get('/protected-route', function () {
+        return response()->json(['message' => 'You are authorized'], 200);
     });
 });
+    
+
 
 Route::post('/send-invite', [EmailController::class, 'sendEmail']);
 
-Route::post('login', [AuthController::class, 'login']);
