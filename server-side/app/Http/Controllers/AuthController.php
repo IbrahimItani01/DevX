61fd514c62805d042b0ce8dae45dc446e32d1432
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-26 22:21:02 +0200

    php<>handled the error in AuthController

--- server-side/app/Http/Controllers/AuthController.php
+++ server-side/app/Http/Controllers/AuthController.php
@@ -13,24 +13,37 @@ class AuthController extends Controller
     // Register API
     public function register(Request $request)
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8',
-        ]);
+        try{
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:8',
+            ]);
+            try{
+                $user = User::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+            }catch(\Exception $e){
+                return response()->json([
+                    'message' => 'User already exists',
+                ], 400);
+            }
+    
+            $token = JWTAuth::fromUser($user);
+    
+            return response()->json([
+                'message' => 'User registered successfully!',
+                'token' => $token,
+            ], 201);
 
-        $token = JWTAuth::fromUser($user);
-
-        return response()->json([
-            'message' => 'User registered successfully!',
-            'token' => $token,
-        ], 201);
+        }catch  (\Exception $e){
+            return response()->json([
+                'message' => 'Are you a robot?',
+            ], 400);
+        }
     }
 
     public function login(Request $request)
