8f47edc8ffd6c0db6904e7169aba6c6782e5f24f
LyneSultan <lynesultane@gmail.com>
2024-11-26 23:20:06 +0200

    php<>added getUser api

--- server-side/routes/api.php
+++ server-side/routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Middleware\JwtMiddleware;
 use App\Http\Controllers\FileController;
 use App\Http\Controllers\UploadController;
+use App\Http\Controllers\UserController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -29,3 +30,4 @@ Route::get('/get-files', [FileController::class, 'getFiles']);
 Route::post('/upload', [UploadController::class, 'upload']);
 Route::post('/getFileContent', [UploadController::class, 'getFileContent']);
 
+Route::get('/getUser', [UserController::class, 'getUser']);
