b6a1a3dcc5ece90b4ebfb79bf3f5c59f729b9c00
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-27 03:40:24 +0200

    Edited

--- app/src/utils/echo.js
+++ app/src/utils/echo.js
@@ -1,11 +1,12 @@
 import Echo from "laravel-echo";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Pusher from "pusher-js";
 
 const echo = new Echo({
     broadcaster: "pusher",
-    key: process.env.REACT_APP_PUSHER_APP_KEY, // Add this key to your .env file
-    cluster: process.env.REACT_APP_PUSHER_APP_CLUSTER, // Add this cluster to your .env file
-    forceTLS: true, // Ensure this matches your Pusher setup
+    key: process.env.REACT_APP_PUSHER_APP_KEY,
+    cluster: process.env.REACT_APP_PUSHER_APP_CLUSTER,
+    forceTLS: true, // Use TLS (secure WebSocket connection)
 });
 
 export default echo;
